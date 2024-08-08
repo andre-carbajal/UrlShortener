c805610576a35b94f1bb8eaafd05108a6268e438
andre-carbajal <carbajalvargasandre@gmail.com>
2024-08-07 19:06:06 -0500

    add repository to url repository

--- src/main/java/net/andrecarbajal/urlshortener/domain/url/UrlRepository.java
+++ src/main/java/net/andrecarbajal/urlshortener/domain/url/UrlRepository.java
@@ -1,6 +1,8 @@
 package net.andrecarbajal.urlshortener.domain.url;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
 }
